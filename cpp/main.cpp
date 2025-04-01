26ca41be599a28522063fd261c522d7b59c961c4
Vladimir Zarypov <krre31@gmail.com>
2025-04-01 06:58:10 +0300

    Apply Material style

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,9 +1,12 @@
 #include "core/Application.h"
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QQuickStyle>
 
 int main(int argc, char* argv[]) {
     Application app(argc, argv);
+
+    QQuickStyle::setStyle("Material");
 
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("app", &app);
