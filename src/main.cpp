8fbeaa2678b338949bb3eb8d980e0c74bb8c35af
Vladimir Zarypov <krre31@gmail.com>
2025-04-12 06:12:15 +0300

    Use PROJECT_NAME macros to load QML module

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]) {
         QCoreApplication::exit(EXIT_FAILURE);
     }, Qt::QueuedConnection);
 
-    engine.loadFromModule("shoppinghauer", "Main");
+    engine.loadFromModule(PROJECT_NAME, "Main");
 
     return app.exec();
 }
