54e7c7ab16be5580b1faf5926ceeee80cc018f10
Vladimir Zarypov <krre31@gmail.com>
2025-04-13 18:14:33 +0300

    Replace `QQmlApplicationEngine::loadFromModule()` method by `QQmlApplicationEngine::load()`

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]) {
         QCoreApplication::exit(EXIT_FAILURE);
     }, Qt::QueuedConnection);
 
-    engine.loadFromModule(PROJECT_NAME, "Main");
+    engine.load(QUrl("qrc:/qml/Main.qml"));
 
     return app.exec();
 }
